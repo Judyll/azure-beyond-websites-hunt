668f5ca39ca7c4d5ded3bc0fb94711f90f3b814a
Judyll <judyll.agan@gmail.com>
2020-08-03 19:05:42 +0800

    Added webjobs project.

--- AzureBeyondBasic/Controllers/HomeController.cs
+++ AzureBeyondBasic/Controllers/HomeController.cs
@@ -6,20 +6,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AzureBeyondBasic.Models;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace AzureBeyondBasic.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            string connectionString = _configuration.GetConnectionString("StorageQueue");
+
+            if (!string.IsNullOrWhiteSpace(connectionString))
+            {
+                QueueClient queue = new QueueClient(connectionString, "mystoragequeue");
+                queue.CreateIfNotExists();
+
+                if (queue.Exists())
+                {
+                    queue.SendMessage($"Queue Date: {DateTime.UtcNow.ToShortDateString()}");
+                }
+            }
+
             return View();
         }
 
