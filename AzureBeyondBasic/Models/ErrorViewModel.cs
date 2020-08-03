89a0911a5a5ab0278c9996c0cebde21313d52211
Judyll <judyll.agan@gmail.com>
2020-08-03 15:58:13 +0800

    Initial project set-up

---
+++ AzureBeyondBasic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureBeyondBasic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
